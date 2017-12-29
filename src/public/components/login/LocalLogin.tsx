c27885c9a182147c5f9048817a13dcfe7164e8db
Randy Lee <randy.w.lee@gmail.com>
2017-12-28 20:21:05 -0500

    Local Login
    Working on implementing the bootstrap form validation strategy with react.

--- src/public/components/login/LocalLogin.tsx
+++ src/public/components/login/LocalLogin.tsx
@@ -29,7 +29,14 @@ export class LocalLogin extends React.Component<
     this.setState({ password: event.target.value });
   };
 
-  handleLogin = (event: any) => {};
+  handleLogin = (event: any) => {
+    let form = document.getElementById("localLogin") as any;
+    if (form.checkValidity() === false) {
+        event.preventDefault();
+        event.stopPropagation();
+      }
+      form.classList.add('was-validated');
+  };
 
   handleCreateAccount = (event: any) => {
     event.preventDefault();
@@ -41,15 +48,13 @@ export class LocalLogin extends React.Component<
     this.props.showForgotPassword();
   };
   render() {
-    return (
-      <div>
+    return <div>
         <div className="row">
           <div className="col-lg-3" />
           <div className="col-lg-9">
             <div className="text-primary">
               <span>
-                <i className="fa fa-user" aria-hidden="true" />&nbsp;&nbsp;Login
-                using your local account or&nbsp;
+                <i className="fa fa-user" aria-hidden="true" />&nbsp;&nbsp;Login using your local account or&nbsp;
                 <a href="" onClick={this.handleCreateAccount}>
                   create a new local account
                 </a>&nbsp;to get started!
@@ -58,7 +63,7 @@ export class LocalLogin extends React.Component<
           </div>
         </div>
         <br />
-        <form className="form-horizontal">
+        <form className="form-horizontal" id="localLogin">
           <div className="form-group">
             <div className="row">
               <div className="col-lg-3" />
@@ -68,14 +73,10 @@ export class LocalLogin extends React.Component<
                 </label>
               </div>
               <div className="col-lg-4">
-                <input
-                  className="form-control"
-                  id="email"
-                  type="text"
-                  value={this.state.email}
-                  placeholder="Enter your email address"
-                  onChange={this.handleEmailChange}
-                />
+                <input className="form-control" id="email" required type="text" value={this.state.email} placeholder="Enter your email address" onChange={this.handleEmailChange} />
+                <div className="invalid-feedback">
+                  Email Address is required.
+                </div>
               </div>
             </div>
           </div>
@@ -83,22 +84,15 @@ export class LocalLogin extends React.Component<
             <div className="row">
               <div className="col-lg-3" />
               <div className="col-lg-2">
-                <label
-                  style={styleLocalLoginLabelsPaddingLeft}
-                  htmlFor="password"
-                >
+                <label style={styleLocalLoginLabelsPaddingLeft} htmlFor="password">
                   Password
                 </label>
               </div>
               <div className="col-lg-4">
-                <input
-                  className="form-control"
-                  id="password"
-                  type="password"
-                  value={this.state.password}
-                  placeholder="Enter your password"
-                  onChange={this.handlePasswordChange}
-                />
+                <input className="form-control" id="password" required type="password" value={this.state.password} placeholder="Enter your password" onChange={this.handlePasswordChange} />
+                <div className="invalid-feedback">
+                  Password is required.
+                </div>
               </div>
             </div>
           </div>
@@ -106,12 +100,9 @@ export class LocalLogin extends React.Component<
             <div className="row">
               <div className="col-lg-5" />
               <div className="col-lg-2">
-                <input
-                  id="rememberMe"
-                  type="checkbox"
-                  className="formControl"
-                  title="Remember me"
-                />&nbsp;<label htmlFor="rememberMe">Remember me</label>
+                <input id="rememberMe" type="checkbox" className="formControl" title="Remember me" />&nbsp;&nbsp;<label htmlFor="rememberMe">
+                  Remember me
+                </label>
               </div>
               <div className="col-lg-2">
                 <a href="" onClick={this.handleForgotPassword}>
@@ -129,7 +120,6 @@ export class LocalLogin extends React.Component<
             </div>
           </div>
         </form>
-      </div>
-    );
+      </div>;
   }
 }
