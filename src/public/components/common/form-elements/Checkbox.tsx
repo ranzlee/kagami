c5bfeabd55c8358679202cea3a9f06207bcf58bf
Randy Lee <randy.w.lee@gmail.com>
2018-01-03 16:10:21 -0500

    Component UI
    Removed blue background from custom checkbox.

--- src/public/components/common/form-elements/Checkbox.tsx
+++ src/public/components/common/form-elements/Checkbox.tsx
@@ -2,38 +2,57 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import * as FormControl from "./FormControl";
 
-export interface CheckboxState { }
+export interface CheckboxState extends FormControl.FormControlState {}
 
 export interface CheckboxProps extends FormControl.FormControlProps {
-    inputId: string;
-    label: string;
-    isRequired: boolean;
-    invalidFeedback?: string;
+  inputId: string;
+  label: string;
+  isRequired?: boolean;
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 export class Checkbox extends React.Component<CheckboxProps, CheckboxState> {
-    constructor(props: CheckboxProps) {
-        super(props);
-        this.state = {};
-    }
+  constructor(props: CheckboxProps) {
+    super(props);
+    this.state = { invalidFeedback: this.props.invalidFeedback };
+  }
 
-    render() {
-        let extendedProps = FormControl.FormControlExtendedProperties(this.props);
-        let required = this.props.isRequired ? true : false;
-        return (
-            <>
-            <div className={extendedProps.labelClasses}></div>
-            <div className={extendedProps.formControlClasses}>
-                <div className="checkbox">
-                    <input id={this.props.inputId} type="checkbox" className="custom-control-input" required={required} />
-                    <label htmlFor={this.props.inputId} className="custom-control-label">{this.props.label}</label>
-                    <div className="invalid-feedback">
-                        {this.props.invalidFeedback ? this.props.invalidFeedback : ""}
-                    </div>
-                </div>
-            </div>
-            {extendedProps.children}
-            </>
-        )
+  onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (this.props.onChange) {
+      this.props.onChange(event);
     }
+    if (this.props.onChangeCustomValidation) {
+      FormControl.OnChangeCustomValidation(this, event);
+    }
+  };
+
+  render() {
+    let extendedProps = FormControl.FormControlExtendedProperties(this.props);
+    let required = this.props.isRequired ? true : false;
+    return (
+      <>
+        <div className={extendedProps.labelClasses} />
+        <div className={extendedProps.formControlClasses}>
+          <div className="checkbox">
+            <input
+              id={this.props.inputId}
+              type="checkbox"
+              className="custom-control-input"
+              required={required}
+            />
+            <label
+              htmlFor={this.props.inputId}
+              className="custom-control-label"
+            >
+              {this.props.label}
+            </label>
+            <div className="invalid-feedback">
+              {this.props.invalidFeedback ? this.props.invalidFeedback : ""}
+            </div>
+          </div>
+        </div>
+        {extendedProps.children}
+      </>
+    );
+  }
 }
