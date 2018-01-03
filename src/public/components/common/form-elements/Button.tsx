6eadd492cf9641f7cc37d0e2fdbb1e2a3079fe91
Kevin Mistry <kevinmistry@hotmail.com>
2018-01-03 16:08:56 -0500

    added null/undefined check onclick

--- src/public/components/common/form-elements/Button.tsx
+++ src/public/components/common/form-elements/Button.tsx
@@ -1,19 +1,19 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-export interface ButtonState {}
+export interface ButtonState { }
 
 export interface ButtonProps {
   buttonType: "button" | "submit";
   buttonClassName:
-    | "primary"
-    | "secondary"
-    | "success"
-    | "danger"
-    | "warning"
-    | "info"
-    | "light"
-    | "dark";
+  | "primary"
+  | "secondary"
+  | "success"
+  | "danger"
+  | "warning"
+  | "info"
+  | "light"
+  | "dark";
   buttonText: string;
   buttonFaIconName?: string;
   onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
@@ -27,7 +27,9 @@ export class Button extends React.Component<ButtonProps, ButtonState> {
 
   onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
-    this.props.onClick(event);
+    if (this.props.onClick != null) {
+      this.props.onClick(event);
+    }
   };
 
   render() {
@@ -35,23 +37,23 @@ export class Button extends React.Component<ButtonProps, ButtonState> {
     let iconClasses = "fas " + this.props.buttonFaIconName;
     let innerElement =
       this.props.buttonFaIconName != null &&
-      this.props.buttonFaIconName !== "" ? (
-        <span>
-          <i className={iconClasses} />&nbsp;&nbsp;{this.props.buttonText}
-        </span>
-      ) : (
-        <span>{this.props.buttonText}</span>
-      );
+        this.props.buttonFaIconName !== "" ? (
+          <span>
+            <i className={iconClasses} />&nbsp;&nbsp;{this.props.buttonText}
+          </span>
+        ) : (
+          <span>{this.props.buttonText}</span>
+        );
     let comp =
       this.props.buttonType === "submit" ? (
         <button type="submit" className={buttonClasses}>
           {innerElement}
         </button>
       ) : (
-        <button type="button" className={buttonClasses} onClick={this.onClick}>
-          {innerElement}
-        </button>
-      );
+          <button type="button" className={buttonClasses} onClick={this.onClick}>
+            {innerElement}
+          </button>
+        );
 
     return comp;
   }
