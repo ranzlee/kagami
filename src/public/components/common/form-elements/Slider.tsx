4013f6e5d919ecf9cf8657da8e1cca42088a8c41
Kevin Mistry <kevinmistry@hotmail.com>
2018-01-10 14:10:46 -0500

    test of disable on slider.  works only on first call.  need to hook into the ref

--- src/public/components/common/form-elements/Slider.tsx
+++ src/public/components/common/form-elements/Slider.tsx
@@ -19,7 +19,7 @@ export class Slider extends React.Component<SliderProps, SliderState> {
     this.state = { invalidFeedback: this.props.invalidFeedback };
   }
 
-  instance: HTMLInputElement;
+  instance: HTMLDivElement;
 
   onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (this.props.readOnly != null && this.props.readOnly) {
@@ -51,6 +51,13 @@ export class Slider extends React.Component<SliderProps, SliderState> {
         ? "horizontal"
         : "vertical";
 
+    const sliderDisabled =
+      this.props.disabled != null
+        ? this.props.disabled
+        : this.props.form && this.props.form.props.disabled != null
+          ? this.props.form.props.disabled
+          : false;
+
     var slider = document.getElementById("sliderRegular");
     noUiSlider.create(slider, {
       start: this.props.value,
@@ -65,16 +72,28 @@ export class Slider extends React.Component<SliderProps, SliderState> {
     });
     slider.style.marginTop = "20px";
     (slider as any).noUiSlider.on("change", this.onChange);
+    if (sliderDisabled) {
+      slider.setAttribute("disabled", "true");
+    } else {
+      slider.removeAttribute("disabled");
+    }
   }
 
   render() {
     let extendedProps = FormControl.FormControlExtendedProperties(this.props);
     let required = this.props.required ? true : false;
+    var slider = document.getElementById("sliderRegular");
     return (
       <div className="row form-group">
         <div className={extendedProps.labelClasses}>{this.props.label}</div>
         <div className={extendedProps.formControlClasses}>
-          <div id="sliderRegular" className="slider" />
+          <div
+            ref={instance => {
+              this.instance = instance;
+            }}
+            id="sliderRegular"
+            className="slider"
+          />
           {/* <div className="checkbox">
             <input
               ref={instance => {
